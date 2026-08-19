b358a1a90c64122f33409dea67ae4eacdf096e52
Vennela0811 <Vennela0811>
2026-08-19 14:56:27 +0530

    DAA

--- Practical 5/KnapSack/1_KnapSack.py
+++ Practical 5/KnapSack/1_KnapSack.py
@@ -1,17 +1,21 @@
 # 0/1 KnapSack using Dynamic Programming
 
 def knapsack(wt, val, n, W):
+    
     dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
 
     for i in range(1, n + 1):
         for w in range(1, W + 1):
             if wt[i - 1] <= w:
-                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w])
+                dp[i][w] = max(
+                    val[i - 1] + dp[i - 1][w - wt[i - 1]],
+                    dp[i - 1][w]
+                )
             else:
                 dp[i][w] = dp[i - 1][w]
 
-
     return dp[n][W]
+
 
 
 def main():
@@ -27,7 +31,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
 """
 TC: O(n * W)
